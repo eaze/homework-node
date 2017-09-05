e333d21d5d223d2c774f8b4db62e44d20a67f199
Enrique Santos <me@enriquesantos.net>
2017-09-04 20:06:50 -0700

    download and extract unscoped node packages

--- index.js
+++ index.js
@@ -1,7 +1,99 @@
-'use strict'
+'use strict';
 
-module.exports = downloadPackages
+var async = require('async');
+var Xray = require('x-ray');
+var tar = require('tar');
+var fs = require('fs-extra');
+var wget = require('wget');
+var zlib = require('zlib');
+
+var tgzDir = './tarballs/';
+var pkgDir = './packages/';
+
+module.exports = downloadPackages;
 
 function downloadPackages (count, callback) {
+  captureMostDependedPackages(
+    count,
+    downloadPackageTarballs.bind(this, callback)
+  );
+}
 
+function downloadPackageTarballs(callback, err, results) {
+  async.map(
+    results,
+    downloadPackageTarball,
+    extractPackageTarballs.bind(this, callback)
+  );
+}
+
+function extractPackageTarballs(callback, err, results) {
+  async.map(
+    results,
+    extractPackageTarball,
+    callback
+  );
+}
+
+function captureMostDependedPackages(count, callback) {
+  var x = Xray();
+  x('https://www.npmjs.com/browse/depended', '.package-details', [{
+    name: 'h3 a',
+    version: 'a.type-neutral-1'
+  }])
+    .paginate('.pagination .next@href')
+    .limit(Math.ceil(count/36))
+    (function(err, results) {
+      if (err) throw Error(err);
+      results = results.slice(0, count);
+      console.log(results);
+      callback(null, results);
+    });
+}
+
+function downloadPackageTarball(pkg, callback) {
+  var registryUrl = 'http://registry.npmjs.org/';
+
+  var filename = pkg.name + '-' + pkg.version;
+  var pkgTarballUrl = registryUrl + pkg.name + '/-/' + filename + '.tgz';
+
+  fs.ensureDir(tgzDir, function downloadTgz() {
+    var pkgLocation = tgzDir + filename + '.tgz';
+    var download = wget.download(pkgTarballUrl, pkgLocation);
+
+    download
+      .on('error', function onError(err) {
+        callback(err);
+      })
+      .on('end', function onEnd() {
+        var result = {
+          name: pkg.name,
+          version: pkg.version,
+          location: pkgLocation
+        };
+        console.log('Downloaded ' + result.name + ' to ' + result.location);
+        callback(null, result);
+      });
+  });
+}
+
+function extractPackageTarball(pkg, callback) {
+  var extractDir = pkgDir + pkg.name;
+
+  fs.ensureDir(extractDir, function extractTgz() {
+    fs
+      .createReadStream(pkg.location)
+      .pipe(new zlib.Gunzip())
+      .pipe(tar.x({
+        strip: 1,
+        C: extractDir
+      }))
+      .on('error', function onError(err) {
+        callback(err);
+      })
+      .on('end', function onEnd() {
+        console.log('Extracted ' + pkg.location + ' to ' + extractDir);
+        callback(null, extractDir);
+      });
+    });
 }
