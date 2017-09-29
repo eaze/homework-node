d4008ed1c977322598394625a150f164075acdf5
Joshua Grossberg <joshgrossberg@gmail.com>
2017-09-28 22:01:30 -0700

    comments, cleanup, formatting

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ class Dependencies{
         this.downloadPackages = this.downloadPackages.bind(this);
         this.CMDLINE_MODE = parseInt(process.env.CMDLINE_MODE) || 0
     }
+
     requestCallback(error, response, body) {
         jsdom.env(body, function (err, window) {
             // it is possible to simply call getElementsByClassName('name'), but I believe this
@@ -21,32 +22,43 @@ class Dependencies{
             // package than the package-details class
             let packages = window.document.documentElement.getElementsByClassName('package-details');
             let package_names = [];
-            let offset = this.offset;
-            let names;
             // iterate over each package parsed, populate array of package names
             for ( let i in packages ) {
-                if ( package_names.length  + offset >= this.count ) {
+                if ( package_names.length  + this.offset >= this.count ) {
                     break
                 }
                 let pkg = packages[i];
+
+                // error handling
                 if ( typeof pkg.getElementsByClassName !== 'function' ) {
                     continue;
                 }
-                names = pkg.getElementsByClassName('name');
+                let names = pkg.getElementsByClassName('name');
                 if ( names.length !== 1 ) {
                     console.log( "Error:", names );
                     continue;
                 }
-                package_names.unshift(names[0].href.replace('/package/', ''));
+
+                // ensure extracted string is valid node module
+                // name.  This also sanitizes data/prevents console injection
+                let name = names[0].href.replace('/package/', '');
+                var invalid_name_reg = new RegExp("[^a-z0-9-.@/]","i");
+                if ( invalid_name_reg.test(name) ) {
+                    console.log( "Error:", name, " is invalid node module name " );
+                    continue;
+                }
+                    
+                package_names.push(name);
             }
     
-            let new_offset = offset + package_names.length;
+            let new_offset = this.offset + package_names.length;
+            // if count > total amount of packages scraped, grab next page
             if ( new_offset < this.count ) {
                 let url = 'https://www.npmjs.com/browse/depended?offset='+new_offset;
                 request(url, this.requestCallback.bind(this));
             }
     
-            this.downloadAndUntarPackages( package_names, offset );
+            this.downloadAndUntarPackages( package_names, this.offset );
             this.offset = new_offset;
         }.bind(this));
     }
@@ -56,10 +68,8 @@ class Dependencies{
     // I was recommended by interviewer.
     downloadAndUntarPackages( package_names, packages_downloaded ) {
         if ( this.CMDLINE_MODE ) {
-            console.log( "JG: CMDLINE_MODE" );
             this.downloadAndUntarPackagesCmdline( package_names, packages_downloaded );
         } else {
-            console.log( "JG: STREAM_MODE" );
             this.downloadAndUntarPackagesStream( package_names, packages_downloaded );
         }
     }
@@ -71,7 +81,6 @@ class Dependencies{
             child.stdout.on('data', (data) => { 
                 // note double quotes, to ensure proper handling of special chars
                 let cmd = 'mkdir -p "packages/'+pkg_name+ '"; '+TAR+' -xzvf '+data.toString().trim() + ' -C "packages/'+pkg_name+'" --strip-components=1';
-            
                 // spawn seems to fail for large tarballs, for example rxjs
                 let untar = cp.exec(cmd);
                 untar.on('error', ( err ) => { console.log( "Error: ", err ) } );
@@ -88,6 +97,11 @@ class Dependencies{
     downloadAndUntarPackagesStream( package_names, packages_downloaded ) {
         for ( let i in  package_names ) {
             let pkg_name = package_names[i];
+            // npm pack <package> gives us the tarball of the package
+            // I suppose if I wanted to I could call npm view <package>,
+            // get the tarball url and then call request with the url,
+            // but this seems unnecessary and also requires an additional
+            // network call
             let child = cp.spawn(NPM, [ 'pack', pkg_name ] );
             child.stdout.on('data', (data) => { 
                 let mkdirp = cp.exec('mkdir -p "packages/'+pkg_name+'"')
@@ -95,7 +109,7 @@ class Dependencies{
                     fs.createReadStream(data.toString().trim()).pipe(
                         tar.x({
                             strip: 1,
-                            C: 'packages/'+pkg_name // alias for cwd:'some-dir', also ok 
+                            C: 'packages/'+pkg_name
                         })).on( 'close', _ => { 
                             packages_downloaded += 1;
                             if ( packages_downloaded >= this.count ) {
--- test.js
+++ test.js
@@ -7,62 +7,69 @@ const folderSize = require('get-folder-size')
 const download = require('./')
 
 test('download', function (t) {
-  t.plan(3)
+    t.plan(3)
 
-  const COUNT = parseInt(process.env.COUNT, 10) || 10
+    const COUNT = parseInt(process.env.COUNT, 10) || 10
 
-  series([
-    (callback) => download(COUNT, callback),
-    verifyCount,
-    verifySize,
-    verifyLodash
-  ], t.end)
+    series([
+        (callback) => download(COUNT, callback),
+        verifyCount,
+        verifySize,
+        verifyLodash
+    ], t.end)
 
-  function verifyCount (callback) {
-    fs.readdir('./packages', function (err, files) {
-      if (err) return callback(err)
-      // Filter .gitignore and other hidden files
-      // also filter out dirs for scoped modules,
-      // they will be accounted for later
-      let unscoped_dirs = files.filter((file) => !/^[\@\.]/.test(file))
-      let num_dirs = unscoped_dirs.length;
-      let scoped_dirs = files.filter((file) => /^[\@]/.test(file));
-      if ( scoped_dirs.length === 0 ) {
-        t.equal(num_dirs, COUNT, `has ${COUNT} files`)
-        callback()
-      }
-      else {
-        let scoped_dirs_scraped = 0;
-        for ( let i in scoped_dirs ) {
-          let scoped_dir = scoped_dirs[i];
-          fs.readdir('./packages/'+scoped_dir, function (err, files) {
+    function verifyCount (callback) {
+        fs.readdir('./packages', function (err, files) {
             if (err) return callback(err)
-            // assumes no second degree scoped modules, i sont think that this is supported
-            num_dirs += files.filter((file) => !/^[\@\.]/.test(file)).length
-            scoped_dirs_scraped += 1;
-            if ( scoped_dirs_scraped >= scoped_dirs.length ) {
-              t.equal(num_dirs, COUNT, `has ${COUNT} files`)
-              callback()
+            // Filter .gitignore and other hidden files
+            // also filter out dirs for scoped modules,
+            // they will be accounted for later
+            let unscoped_dirs = files.filter((file) => !/^[\@\.]/.test(file))
+            let num_dirs = unscoped_dirs.length;
+
+            // scoped dirs must be handled differently,
+            // for example, there are many popular modules
+            // under the scope @angular: @angular/core, @angular/http etc..
+            // for these we cannot simply count the top level dir ( there is
+            // only one ), we have to go into this dir and count the subdirs
+            let scoped_dirs = files.filter((file) => /^[\@]/.test(file));
+            if ( scoped_dirs.length === 0 ) {
+                // if no scoped dirs we re done
+                t.equal(num_dirs, COUNT, `has ${COUNT} files`)
+                callback()
             }
-          })
-        }  
-      }  
-    })
-  }
+            else {
+                // scrape the subdirs of each scope
+                let scoped_dirs_scraped = 0;
+                for ( let i in scoped_dirs ) {
+                    let scoped_dir = scoped_dirs[i];
+                    fs.readdir('./packages/'+scoped_dir, function (err, files) {
+                        if (err) return callback(err)
+                        num_dirs += files.filter((file) => !/^[\@\.]/.test(file)).length
+                        scoped_dirs_scraped += 1;
+                        if ( scoped_dirs_scraped >= scoped_dirs.length ) {
+                            t.equal(num_dirs, COUNT, `has ${COUNT} files`)
+                            callback()
+                        }
+                    })
+                }  
+            }  
+        })
+    }
 
-  // FIXME: this is inaccurate when scoped packages are downloaded:
-  // https://docs.npmjs.com/misc/scope#requiring-scoped-packages
-  function verifySize (callback) {
-    folderSize('./packages', function (err, size) {
-      if (err) return callback(err)
-      t.ok(size / 1024 > 5 * COUNT, 'min 5k per package')
-      callback()
-    })
-  }
+    // FIXME: this is inaccurate when scoped packages are downloaded:
+    // https://docs.npmjs.com/misc/scope#requiring-scoped-packages
+    function verifySize (callback) {
+        folderSize('./packages', function (err, size) {
+            if (err) return callback(err)
+            t.ok(size / 1024 > 5 * COUNT, 'min 5k per package')
+            callback()
+        })
+    }
 
-  function verifyLodash (callback) {
-    const _ = require('./packages/lodash')
-    t.equal(typeof _.map, 'function', '_.map exists')
-    callback()
-  }
+    function verifyLodash (callback) {
+        const _ = require('./packages/lodash')
+        t.equal(typeof _.map, 'function', '_.map exists')
+        callback()
+    }
 })
