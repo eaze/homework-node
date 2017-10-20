de909d850d049a4456886995d9c501fb64ea9ab9
Chris Oyler <christopher.oyler@gmail.com>
2017-10-20 14:16:09 -0700

    add jsdoc to dev dependencies as well as inline documentation, adds items to .gitignore, cleans up some code

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * @file index.js
+ * @author Christopher Oyler <christopher.oyler@gmail.com>
+ * @date 10/20/2017
+ * @desc program to download tgz packages from npm registry and unpack them
+ */
+
 'use strict'
 
 const fs = require('fs');
@@ -8,6 +15,14 @@ const popularUrl = 'https://www.npmjs.com/browse/depended?offset=';
 
 module.exports = downloadPackages;
 
+/**
+ * @function downloadPackages - primary function for this program and the only export
+ * @param {number} count - the number of popular packages to download
+ * @param {function} callback - a non-optional callback function to fire when complete
+ * @fires getMatches
+ * @fires clean
+ * @fires downloadAndExpand
+ */
 function downloadPackages(count, callback) {
   const pages = Math.ceil(count / 36);
 
@@ -26,7 +41,12 @@ function downloadPackages(count, callback) {
   });
 }
 
-
+/**
+ * @function getMatches - a scraping function to pull in html from npmjs.com and select popular packages
+ * @param {string} url - a preformatted url string with query param for npm pagination
+ * @param {function} cb - a non-optional callback function to fire when complete
+ * @returns {array} matches - an array of strings of matched packages
+ */
 function getMatches(url, cb) {
   request(url, (err, resp, html) => {
     if (err) return callback(err);
@@ -38,6 +58,10 @@ function getMatches(url, cb) {
   });
 }
 
+/**
+ * @function clean - a function to clean ./packages prior to downloading new items
+ * @param {function} cb - a non-optional callback to fire when complete
+ */
 function clean(cb) {
   fs.readdir('./packages', (err, files) => {
     if (err) return cb(err);
@@ -53,25 +77,38 @@ function clean(cb) {
   });
 }
 
+/**
+ * @function downloadAndExpand - bread and butter, does most of the work for this program
+ * it fires a series of child_process and file system calls using the npm cli and native methods
+ * to download the tgz of a package, unpack that, and remove the tgz when finished
+ * @param {array} matchArr - a flattened array of matched package strings
+ * @param {function} cb - a non-optional callback to fire when complete
+ */
 function downloadAndExpand(matchArr, cb) {
+  //limit concurrency to 10 downloads
   async.eachLimit(matchArr, 10, (match, cb) => {
     const pkgName = match.split('/')[1].split('">')[0];
     const opts = { cwd: './packages' };
     let tarName;
 
     async.series([
+      //npm command to get the tgz
       (cb) => cp.exec(`npm pack ${pkgName}`, opts, (err, sout, serr) => {
         if (err) return cb(err);
         tarName = sout.trim();
         cb(null);
       }),
+      //create the directory for the package name, without the version info
       (cb) => fs.mkdir(`./packages/${pkgName}`, cb),
+      //unpack the tarball, and strip the "package" folder it would otherwise create
       (cb) => cp.exec(`tar -xvf ${tarName} -C ${pkgName} --strip-components 1`, opts, cb),
+      //remove the tgz file when complete
       (cb) => cp.exec(`rm -rf ${tarName}`, opts, cb)
     ], (err) => {
       if (err) return cb(err);
       cb(null);
     });
+
   }, (err) => {
     if (err) return cb(err);
     cb(null);
